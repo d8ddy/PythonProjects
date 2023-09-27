87560ecd2e2827fecce58a6739eeb5237c057745
warsi1 <syedhussamwarsi@gmail.com>
2023-09-27 20:30:15 +0100

    fixed trained_file function to store .txt data

--- Bots/ChatBot.py
+++ Bots/ChatBot.py
@@ -4,34 +4,42 @@ from difflib import get_close_matches
 def trained_file(file_path: str) -> dict:
     chat_data = {}
     try:
-        with open(file_path) as file:
-            for line in file:
-                data = [line.rsplit(' ', 1)
-                        for line in line.split('\n') if line]
-                chat_data = {key: str(value) for key, value in data}
-                print(chat_data)
+        with open(file_path, 'r') as file:
+            data_by_line: str = file.read().split('\n')
+            lines: list[str] = []
+            for line in data_by_line:
+                l = line.split('\t')
+                l = tuple(l)
+                print(l)
+                lines.append(l)
+            for key, val in lines:
+                chat_data[key] = val
+            # print(chat_data)
     except Exception as e:
         print(f'Error: {e}')
-
     return chat_data
 
 
 def get_best_match(user_question: str, questions: dict) -> str | None:
     questions: list[str] = [q for q in questions]
     matches: list = get_close_matches(
-        user_question, questions, n=1, cutoff=0.6)
+        user_question, questions, n=1, cutoff=0.40)
     if matches:
         return matches[0]
 
 
 def chat_bot(knowledge: dict):
-    user_input: str = input('You: ')
-    best_match: str | None = get_best_match(user_input, knowledge)
     while True:
+        user_input: str = input('You: ')
+        best_match: str | None = get_best_match(user_input, knowledge)
+
         if answer := knowledge.get(best_match):
             print(f'Bot: {answer}')
         else:
             print('Bot: I do not understand :(')
+
+        if user_input.lower() == 'EXIT':
+            break
 
 
 if __name__ == '__main__':
